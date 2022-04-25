1c5f4463cfd46c364dedf397e19f04511d2e3175
amaneth <amankassahun190@gmail.com>
2022-04-25 21:26:37 +0000

    add model name as figure name

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ transform = transforms.Compose([transforms.RandomResizedCrop(224),
                                        transforms.Normalize([0.485, 0.456, 0.406],
                                                             [0.229, 0.224, 0.225])])
 
-def plot(train_loss, val_loss):
+def plot(train_loss, val_loss, model_name):
     plt.title("Training results: Loss")
     plt.plot(val_loss,label='val_loss')
     plt.plot(train_loss, label="train_loss")
     plt.legend()
-    plt.savefig("./figures/train_loss.png")
+    plt.savefig("./figures/train_"+model_name+".png")
     plt.show()
